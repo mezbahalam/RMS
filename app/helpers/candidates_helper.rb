3b81f89f4077c38e7d98213dcff56d92de1e01bd
lra-wtag <laila.raha@welldev.io>
2019-01-08 10:59:34 +0600

    resolving reviews

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -6,18 +6,16 @@ module CandidatesHelper
   end
 
   def age(dob)
-    if dob.try(:to_date)
+    if valid_date = dob.try(:to_date)
       today = Date.today
-      age = today.year - dob.year
-      age -= 1 if dob.strftime('%m%d').to_i > today.strftime('%m%d').to_i
+      age = today.year - valid_date.year
+      age -= 1 if valid_date.strftime('%m%d').to_i > today.strftime('%m%d').to_i
       age
     end
   end
 
   def valid_age?(dob)
     applicant_age = age(dob)
-    if applicant_age > 15
-      true
-    end
+    applicant_age > 15
   end
 end

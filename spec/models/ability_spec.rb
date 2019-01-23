ea701666f238349d3e5bc838def2d60a1a8cba0a
lra-wtag <laila.raha@welldev.io>
2019-01-23 10:02:12 +0600

    pronto errors solving

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -44,12 +44,8 @@ RSpec.describe Ability, type: :model do
       FactoryBot.create(:user, role: :applicant)
     end
 
-    let(:another_user) do
-      FactoryBot.create(:user, role: :applicant)
-    end
-
     let(:first_candidate) { FactoryBot.create(:candidate, user_id: user.id) }
-    let(:second_candidate) { FactoryBot.create(:candidate, id: first_candidate.id + 1, user_id: another_user.id) }
+    let(:second_candidate) { FactoryBot.create(:candidate, id: first_candidate.id + 1)}
 
     let(:candidate_job) do
       FactoryBot.build(:candidate_job,

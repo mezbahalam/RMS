0b0871373ddecfcf2285b6418f7c3fcd0075f2e6
aha-wtag <alex.hasdak@welldev.io>
2019-02-14 11:01:03 +0600

    scss, help controller, spec

---
+++ spec/controllers/helps_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+RSpec.describe HelpsController, type: :controller do
+  let!(:user) do
+    FactoryBot.create(:user, role: :applicant)
+  end
+
+  before do
+    sign_in_as user
+  end
+
+  describe 'Get #show' do
+    it 'renders the applicants to show' do
+      get :show
+      expect(response).to render_template :show
+    end
+  end
+end

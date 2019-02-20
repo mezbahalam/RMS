886a2ffeb13d5cbc46e6fd1afc2cde7e24e94292
aha-wtag <alex.hasdak@welldev.io>
2019-02-20 14:04:07 +0600

    spec and scss review resolved

--- spec/controllers/jobs/calendars_controller_spec.rb
+++ spec/controllers/jobs/calendars_controller_spec.rb
@@ -1,44 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Jobs::CalendarsController, type: :controller do
-  let!(:user_1) { FactoryBot.create(:user, role: :admin) }
-  let!(:user_2) { FactoryBot.create(:user, role: :applicant) }
-  let!(:job_1) { FactoryBot.create(:job) }
-  let!(:candidate_1) { FactoryBot.create(:candidate, user_id: user_2.id) }
-  let!(:can_job) { FactoryBot.create(:candidate_job, candidate_id: candidate_1.id, job_id: job_1.id) }
+  let!(:first_user) { FactoryBot.create(:user, role: :admin) }
+  let!(:second_user) { FactoryBot.create(:user, role: :applicant) }
+  let!(:job_one) { FactoryBot.create(:job) }
+  let!(:candidate_one) { FactoryBot.create(:candidate, user_id: second_user.id) }
+  let!(:candidate_job_one) { FactoryBot.create(:candidate_job, candidate_id: candidate_one.id, job_id: job_one.id) }
 
   context 'User is admin' do
     describe 'GET #show as admin user' do
       before do
-        sign_in_as user_1
-        get :show
+        sign_in_as first_user
       end
 
-      let!(:job_2) do
+      let!(:job_two) do
         FactoryBot.create(:job, title: 'junior_software_engineer')
       end
 
       it 'populates an array of all jobs' do
-        expect(assigns(:calendars)).to match_array [job_1, job_2]
+        get :show
+        expect(assigns(:calendars)).to match_array [job_one, job_two]
       end
 
       it 'renders show page' do
+        get :show
         expect(response).to render_template :show
       end
     end
   end
+
   context 'User is applicant' do
     describe 'GET #show as applicant' do
       before do
-        sign_in_as user_2
-        get :show
+        sign_in_as second_user
       end
 
       it 'populates an array of all the job applied by the candidate' do
-        expect(assigns(:calendars)).to match_array [can_job]
+        get :show
+        expect(assigns(:calendars)).to match_array [candidate_job_one]
       end
 
       it 'renders show page' do
+        get :show
         expect(response).to render_template :show
       end
     end
